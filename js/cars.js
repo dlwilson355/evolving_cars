13af70557bbe5bba0b1a4e8b0e12f4d4da694bd8
Daniel Wilson <dlwilson355@gmail.com>
2018-11-08 17:59:54 -0500

    Implmented concave car shapes.

--- js/cars.js
+++ js/cars.js
@@ -63,81 +63,36 @@ class Track {
 }
 
 
-// A car chassis constructed from a convex object
-class CarChassisConvex {
+class CarChassis {
     constructor(options) {
         this.position = options["position"];
-        this.vert = this.get_verticies(options['angles'], options['lengths']);
+        this.angles = options['angles'];
+        this.lengths = options['lengths'];
+        this.vert = this.get_verticies(this.angles, this.lengths);
         this.body = new p2.Body({
             mass: 1,
             position: this.position
         });
-        this.shape = new p2.Convex({ vertices: this.vert });
-        this.shape.collisionGroup = CHASSIS;
-        this.shape.collisionMask = TRACK;
-        this.body.addShape(this.shape);
-    }
-
-    // we select random verticies to determine the shape of the convex chassis
-    get_random_verticies() {
-        var size = 1;
-        var vertices = [];
-        for(var i=0, N=4; i<N; i++){
-            var a = 2*Math.PI / N * i;
-            var vertex = [Math.random()*size*Math.cos(a), Math.random()*size*Math.sin(a)]; // Note: vertices are added counter-clockwise
-            vertices.push(vertex);
+        this.body.fromPolygon(this.vert);
+        for (var i=0; i<this.body.shapes.length; i++) {
+            this.body.shapes[i].collisionGroup = CHASSIS;
+            this.body.shapes[i].collisionMask = TRACK;
         }
-        return vertices
-    }
-
-    // we need to sort the verticies in counterclockwise order or else p2 throws an exception
-    get_sorted_verticies(verts) {
-        var radians = [];
-        for (var i=0; i < verts.length; i++) {
-            var radian = Math.atan2(verts[i][1], verts[i][0]);
-            if (radian < 0) {
-                radian += 2*Math.PI;
-            }
-            radians.push(radian);
-        }
-        var sorted_verticies = [];
-        // TODO: potential issue if two radians are equal
-        for (var i=0; i < verts.length; i++) {
-            var index = radians.indexOf(Math.min.apply(Math, radians));
-            sorted_verticies.push(verts[index]);
-            radians[index] = 10;
-        }
-        return sorted_verticies
     }
 
     get_verticies(angles, lengths) {
-        [angles, lengths] = this.fix_errors(angles, lengths);
-        var angles = angles.sort();
-        var verts = [];
-        for (var i=0; i<angles.length; i++) {
-            var x = lengths[i] * Math.cos(angles[i]);
-            var y = lengths[i] * Math.sin(angles[i]);
-            verts.push([x, y]);
+        var sorted_angles = angles.sort();
+        var vs = [];
+        for (var i=0; i<sorted_angles.length; i++) {
+            var x = lengths[i] * Math.cos(sorted_angles[i]);
+            var y = lengths[i] * Math.sin(sorted_angles[i]);
+            vs.push([x, y]);
         }
-        return verts;
+        return vs;
     }
 
-    // a temporary functions to prevent exceptions thrown by the renderer if verticies are two close to the origion or if their angles aren't far enough apart
-    // we will need to find a better solution to this
-    fix_errors(angles, lengths) {
-        var prev_angle = angles[3];
-        for (var i=0; i<lengths.length; i++) {
-            // fix the exception that occurs if the length is too small
-            if (lengths[i] < 0.5) {
-                lengths[i] = 0.5;
-            }
-            // fix the exception that occurs if the angles are too close
-            if (Math.abs(angles[i] - prev_angle) < 0.4) {
-                angles = [0, .5*Math.PI, Math.PI, 1.5*Math.PI];
-            }
-            prev_angle = angles[i];
-        }
-        return [angles, lengths]
+    get_verticy(verticy_number, angles, lengths) {
+        return [lengths[verticy_number] * Math.cos(angles[verticy_number]), lengths[verticy_number] * Math.sin(angles[verticy_number])]
     }
 
     add_to_world(world) {
@@ -151,16 +106,19 @@ class CarChassisConvex {
     }
 
     get_back_wheel_position() {
-        return [this.position[0] + this.vert[2][0], this.position[1] + this.vert[2][1]]
+        var verticy = this.get_verticy(2, this.angles, this.lengths);
+        return [this.position[0] + verticy[0], this.position[1] + verticy[1]]
     }
 
     get_front_wheel_position() {
-        return [this.position[0] + this.vert[3][0], this.position[1] + this.vert[3][1]]
+        var verticy = this.get_verticy(3, this.angles, this.lengths);
+        return [this.position[0] + verticy[0], this.position[1] + verticy[1]]
     }
 
     attach_back_wheel(wheel) {
+        var verticy = this.get_verticy(2, this.angles, this.lengths);
         var revolute = new p2.RevoluteConstraint(this.body, wheel.body, {
-            worldPivot: [this.position[0] + this.vert[2][0], this.position[1] + this.vert[2][1]],
+            worldPivot: [this.position[0] + verticy[0], this.position[1] + verticy[1]],
             collideConnected: false
         });
         revolute.enableMotor();
@@ -173,78 +131,9 @@ class CarChassisConvex {
     }
 
     attach_front_wheel(wheel) {
+        var verticy = this.get_verticy(3, this.angles, this.lengths);
         var revolute = new p2.RevoluteConstraint(this.body, wheel.body, {
-            worldPivot: [this.position[0] + this.vert[3][0], this.position[1] + this.vert[3][1]],
-            collideConnected: false
-        });
-        this.world.addConstraint(revolute);
-
-        this.front_revolute = revolute;
-    }
-}
-
-
-class CarChassisLine {
-    constructor(options) {
-        this.position = options["position"];
-        this.length = options["length"];
-        this.height = options["height"];
-
-        this.body = new p2.Body({
-            mass: 1,
-            position: this.position
-        });
-        this.shape = new p2.Box({
-            width: this.length,
-            height: this.height
-        });
-        this.shape.collisionGroup = CHASSIS;
-        this.shape.collisionMask = TRACK;
-        this.body.addShape(this.shape);
-    }
-
-    add_to_world(world) {
-        world.addBody(this.body);
-        this.world = world;
-    }
-
-    // To fix the camera
-    get_center_body() {
-        return this.body;
-    }
-
-    get_back_wheel_position() {
-        return [this.body.position[0] - 0.5 * this.length,
-                this.body.position[1]]
-    }
-
-    get_front_wheel_position() {
-        return [this.body.position[0] + 0.5 * this.length,
-                this.body.position[1]]
-    }
-
-    attach_back_wheel(wheel) {
-        var revolute = new p2.RevoluteConstraint(this.body, wheel.body, {
-            localPivotA: [-0.5 * this.length, 0],
-            localPivotB: [0, 0],
-            collideConnected: false
-        });
-        revolute.enableMotor();
-        // Rotational speed in radians per second
-        revolute.setMotorSpeed(wheel.speed);
-
-        this.world.addConstraint(revolute);
-
-        this.back_revolute = revolute;
-    }
-
-    attach_front_wheel(wheel) {
-        // make the front wheel heavier so that it can't be
-        // pushed up by high speed of the back wheel
-        wheel.body.mass = 1;
-        var revolute = new p2.RevoluteConstraint(this.body, wheel.body, {
-            localPivotA: [0.5 * this.length, 0],
-            localPivotB: [0, 0],
+            worldPivot: [this.position[0] + verticy[0], this.position[1] + verticy[1]],
             collideConnected: false
         });
         this.world.addConstraint(revolute);
@@ -284,85 +173,6 @@ class Car {
         // bw - back wheel, fw - front wheel
         this.radius_bw = options["radius_bw"];
         this.radius_fw = options["radius_fw"];
-        this.length = options["length"];
-        this.speed = 3;
-        if ("speed" in options) {
-            this.speed = options["speed"]
-        }
-        this.removed = false;
-    }
-
-    add_to_world(world) {
-        this.world = world;
-
-        // x=2 not to hit the first platform
-        var car_position = [2, 0.5];
-        var chassis_height = 0.05;
-
-        // Create chassis for our car
-        var chassis = new CarChassisLine({
-            position: car_position,
-            height: chassis_height,
-            length: this.length
-        })
-        chassis.add_to_world(world);
-
-        // Create wheels
-        var back_wheel = new CarWheel({
-            position: chassis.get_back_wheel_position(),
-            radius: this.radius_bw,
-            speed: this.speed,
-        });
-        back_wheel.add_to_world(world);
-
-        var front_wheel = new CarWheel({
-            position: chassis.get_front_wheel_position(),
-            radius: this.radius_fw
-        });
-        front_wheel.add_to_world(world);
-
-        chassis.attach_front_wheel(front_wheel);
-        chassis.attach_back_wheel(back_wheel);
-
-        this.chassis = chassis;
-        this.back_wheel = back_wheel;
-        this.front_wheel = front_wheel;
-
-        this.bodies = [this.chassis.body, this.back_wheel.body, this.front_wheel.body]
-    }
-
-    remove_from_world() {
-        this.world.removeConstraint(this.chassis.back_revolute);
-        this.world.removeConstraint(this.chassis.front_revolute);
-        this.world.removeBody(this.chassis.body);
-        this.world.removeBody(this.back_wheel.body);
-        this.world.removeBody(this.front_wheel.body);
-
-        this.removed = true;
-    }
-
-    has_body(body) {
-        return this.bodies.includes(body)
-    }
-
-    get_position() {
-        return this.chassis.body.position;
-    }
-
-    // To fix the camera
-    get_center_body() {
-        return this.chassis.get_center_body();
-    }
-}
-
-
-// Daniel's new class for creating cars with convex chassis
-// I am leaving the regular car class for now.
-class ConvexCar {
-    constructor(options) {
-        // bw - back wheel, fw - front wheel
-        this.radius_bw = options["radius_bw"];
-        this.radius_fw = options["radius_fw"];
         this.speed = 3;
         [this.angles, this.lengths] = this.get_angles_and_lengths(options["options"])
         if ("speed" in options) {
@@ -389,7 +199,7 @@ class ConvexCar {
         var car_position = START_POSITION;
 
         // Create chassis for our car
-        var chassis = new CarChassisConvex({
+        var chassis = new CarChassis({
             position: START_POSITION,
             angles: this.angles,
             lengths: this.lengths
